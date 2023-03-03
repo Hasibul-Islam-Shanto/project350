4c66bc7e8adc23ce43af701c9b4c676c74039423
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2023-03-03 22:00:46 +0600

    cards add

--- frontend/src/Components/Home/Cards/cardItems.js
+++ frontend/src/Components/Home/Cards/cardItems.js
@@ -1,3 +1,20 @@
-export const cardItems = [
-    
-]
+export const bus = [
+  {
+    id: 1,
+    date: "",
+    from: "",
+    to: "",
+    price: "",
+    image: "",
+  },
+];
+export const train = [
+  {
+    id: 1,
+    date: "",
+    from: "",
+    to: "",
+    price: "",
+    image: "",
+  },
+];
